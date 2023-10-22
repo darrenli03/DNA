6e84616efde2fda74c2fcb79afa5dc796fdb9757
darrenli03 <71399017+darrenli03@users.noreply.github.com>
2023-10-22 17:27:31 -0400

    commented main method

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -18,25 +18,25 @@ public class LinkStrand implements IDnaStrand {
         initialize(s);
     }
 
-    public static void main(String[] args) {
-        LinkStrand bruh = new LinkStrand("bruh1");
-        bruh.append("moment1");
-        bruh.append("bruh2");
-
-        int x = (int) bruh.size() - 1;
-        while(x>= 0){
-            System.out.println(bruh.charAt(x));
-            x--;
-        }
-        bruh.myCurrent = bruh.myFirst;
-        System.out.println(bruh.myCurrent.info);
-        System.out.println(bruh.myCurrent.next.info);
-        System.out.println(bruh.charAt(14));
-        System.out.println(bruh.charAt(14));
-
-
-
-    }
+//    public static void main(String[] args) {
+//        LinkStrand bruh = new LinkStrand("bruh1");
+//        bruh.append("moment1");
+//        bruh.append("bruh2");
+//
+//        int x = (int) bruh.size() - 1;
+//        while(x>= 0){
+//            System.out.println(bruh.charAt(x));
+//            x--;
+//        }
+//        bruh.myCurrent = bruh.myFirst;
+//        System.out.println(bruh.myCurrent.info);
+//        System.out.println(bruh.myCurrent.next.info);
+//        System.out.println(bruh.charAt(14));
+//        System.out.println(bruh.charAt(14));
+//
+//
+//
+//    }
 
     private class Node {
         private String info;
